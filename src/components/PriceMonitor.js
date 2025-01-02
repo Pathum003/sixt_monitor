0831f5c77da0d97a397f9d05ca11a260e0f11651
Pathum <pathum00003@gmail.com>
2025-01-02 00:36:31 -0800

    Add location finder functionality

--- src/components/PriceMonitor.js
+++ src/components/PriceMonitor.js
@@ -1,6 +1,5 @@
 'use client';
 import React, { useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 export default function PriceMonitor() {
   const [locations, setLocations] = useState([]);
